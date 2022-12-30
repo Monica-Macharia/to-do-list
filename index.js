64df2da7e7b8c00757c2ad54d391329fc40d49cd
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-12-30 08:12:55 +0300

    current day changes

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function changeDay(newDay){
-    let day = document.querySelector(".day")
-    day.innerText = newDay
-}
-changeDay("Sunday")
+// function changeDay(newDay){
+//     let day = document.querySelector(".day")
+//     day.innerText = newDay
+// }
+// changeDay("Sunday")
+
+//get the current day
+ function currentDay(){
+     const weekday= ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"]
+     let day = new Date()
+     let word = day.getDay()
+     let newDay = (weekday[word])
+     document.querySelector(".day").style.color = "red"
+     document.querySelector(".day").innerText = newDay
+   
+ }
+ currentDay()
+
+// function changeTitle(newTitle){
+//     let h2 =document.querySelector("h2")
+//     h2.textContent = newTitle
+// }
+// changeTitle("Todo List")
